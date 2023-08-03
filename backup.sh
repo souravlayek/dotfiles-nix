f3c628e10d74e50c5a4001b8c304a29661d630ca
Sourav Layek <souravlayek11@gmail.com>
2023-08-04 00:46:17 +0530

    Update on 08-04-2023 00:46:17

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@ mkdir -p ~/dotfiles/{nix,configs}
 cp -r /etc/nixos/* ~/dotfiles/nix/ &
 cp -r ~/.config/* ~/dotfiles/configs &
 git add .
-today=`date +%m-%d-%Y +%T`
+today=`date +%m-%d-%Y`
 now=`date +%T`
 git commit -m "Update on $today $now"
 git push
