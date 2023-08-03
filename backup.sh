62cd29f08b24628ca0d7b42aca5f7f1a36631f11
Sourav Layek <souravlayek11@gmail.com>
2023-08-04 00:43:39 +0530

    Update on

--- backup.sh
+++ backup.sh
@@ -2,4 +2,7 @@
 mkdir -p ~/dotfiles/{nix,configs}
 cp -r /etc/nixos/* ~/dotfiles/nix/ &
 cp -r ~/.config/* ~/dotfiles/configs &
-
+git add .
+timeStamp=`date +%m-%d-%Y +%T`
+git commit -m "Update on $timeStamp"
+git push
