7cb0e23eb5477426bf682c3e033dbd2ece01db71
Sourav Layek <souravlayek11@gmail.com>
2023-08-04 00:52:45 +0530

    Update on 08-04-2023 00:52:45

--- backup.sh
+++ backup.sh
@@ -2,8 +2,12 @@
 mkdir -p ~/dotfiles/{nix,configs}
 cp -r /etc/nixos/* ~/dotfiles/nix/ &
 cp -r ~/.config/* ~/dotfiles/configs/ &
+execPath=$(pwd)
+cd ~/dotfiles
 git add .
 today=`date +%m-%d-%Y`
 now=`date +%T`
 git commit -m "Update on $today $now"
 git push
+cd $execPath
+
